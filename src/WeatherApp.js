a6a238372e24646554da0c8a29e870a5fdcd3293
Ash <z0975201508@gmail.com>
2021-03-02 19:45:33 +0800

    init & import emotion,normalize.css

---
+++ src/WeatherApp.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// STEP 1 : 載入 emotion 的 styled 套件
+import styled from '@emotion/styled';
+
+// STEP 2 : 定義帶有 styled 的 component
+const Container = styled.div`
+    background-color: #ededed;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const WeatherCard = styled.div`
+    position: relative;
+    min-width: 360px;
+    box-shadow: 0 1px 3px 0 #999999;
+    background-color: #f9f9f9;
+    box-sizing: border-box;
+    padding: 30px 15px;
+`
+
+const WeatherApp = () => {
+    return (
+        <Container>
+            <WeatherCard>
+                <h1>Weather</h1>
+            </WeatherCard>
+        </Container>
+    );
+};
+
+export default WeatherApp;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WeatherApp from './WeatherApp';
+// 這個 CSS 檔案會作用到全域
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return <WeatherApp />
+}
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
