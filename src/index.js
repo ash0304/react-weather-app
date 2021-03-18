6bac7996a5cfa5cf0ff420b9c96b3530c836f8eb
Ash <z0975201508@gmail.com>
2021-03-18 15:53:14 +0800

    PW設定測試&放入圖檔

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import WeatherApp from './WeatherApp';
 // 這個 CSS 檔案會作用到全域
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+// 引用serviceWorker 
+import * as serviceWorker from './serviceWorker'
 
 function App() {
   return <WeatherApp />
@@ -16,4 +18,5 @@ ReactDOM.render(<App />, rootElement);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+serviceWorker.register();
